96a3a8b001c8eeb54bee2d8d15c8550b462a3b1f
Tawanda Kembo <tkembo@gmail.com>
2013-05-23 11:32:53 +0200

    now using app variables for db settings

--- inc/config.php
+++ inc/config.php
@@ -11,10 +11,10 @@
 	$GA_PIXEL = "ga.php";
 	
 	//DB Settings
-	$hostname_mxit_devilsdictionary = "us-cdbr-azure-west-b.cleardb.com";
-	$database_mxit_devilsdictionary = "mxitdevilsdictionary";
-	$username_mxit_devilsdictionary = "b5799a500e83b2";
-	$password_mxit_devilsdictionarys = "8f65b047";
+	$hostname_mxit_devilsdictionary = getenv('DB_HOST');
+	$database_mxit_devilsdictionary = getenv('DB_NAME');
+	$username_mxit_devilsdictionary = getenv('DB_USER');
+	$password_mxit_devilsdictionarys = getenv('DB_PASS');
 	$mxit_devilsdictionary = mysql_pconnect($hostname_mxit_devilsdictionary, $username_mxit_devilsdictionary, $password_mxit_devilsdictionarys) or trigger_error(mysql_error(),E_USER_ERROR); 
 	
 ?>
